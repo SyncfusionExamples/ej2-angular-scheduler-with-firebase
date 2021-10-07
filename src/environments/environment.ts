92ed7b498027386e574f4887a373995c598b6d61
vinithadevi.murugan <Vinitha1995>
2021-10-07 18:31:23 +0530

    sample(EJ2-37514): firebase sample updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAF4lNZ5WtAsXSpfFLlMjD76ij3ykkV71Y",
+    authDomain: "angular-84f46.firebaseapp.com",
+    databaseURL: "https://angular-84f46.firebaseio.com",
+    projectId: "angular-84f46",
+    storageBucket: "angular-84f46.appspot.com",
+    messagingSenderId: "63615603270",
+    appId: "1:63615603270:web:946140011c5410c96bf047",
+    measurementId: "G-4JSCEMTWJ6"
+  }
 };
 
 /*
